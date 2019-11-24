b4ad3c13a9636ba66bf37150147cad4270cb8410
Yutsuten <mateus.etto@gmail.com>
2019-11-24 19:14:56 +0900

    Refactor settings.py

--- lifedrain/settings.py
+++ lifedrain/settings.py
@@ -12,6 +12,8 @@ class Settings(object):
     for configurating the add-on.
     '''
     _qt = None
+    _form = None
+    _row = None
 
     def __init__(self, qt):
         self._qt = qt
@@ -20,34 +22,24 @@ class Settings(object):
         '''
         Appends LifeDrain tab to Global Settings dialog.
         '''
+        self._form = form
+        self._row = 0
+
         form.lifedrain_widget = self._qt.QWidget()
         form.lifedrain_layout = self._gui_settings_setup_layout(form.lifedrain_widget)
-        row = 0
-        self._create_label(form, row, '<b>Bar behaviour</b>')
-        row += 1
-        self._create_check_box(form, row, 'stopOnAnswer', 'Stop drain on answer shown')
-        row += 1
-        self._create_check_box(form, row, 'disableAddon', 'Disable Life Drain (!)')
-        row += 1
-        self._create_label(form, row, '<b>Bar style</b>')
-        row += 1
-        self._create_combo_box(form, row, 'positionList', 'Position', POSITION_OPTIONS)
-        row += 1
-        self._create_spin_box(form, row, 'heightInput', 'Height', [1, 40])
-        row += 1
-        self._create_spin_box(form, row, 'borderRadiusInput', 'Border radius', [0, 20])
-        row += 1
-        self._create_combo_box(form, row, 'textList', 'Text', TEXT_OPTIONS)
-        row += 1
-        self._create_combo_box(form, row, 'styleList', 'Style', STYLE_OPTIONS)
-        row += 1
-        self._create_color_select(form, row, 'bgColor', 'Background color')
-        row += 1
-        self._create_color_select(form, row, 'fgColor', 'Foreground color')
-        row += 1
-        self._create_color_select(form, row, 'textColor', 'Text color')
-        row += 1
-        self._fill_remaining_space(form, row)
+        self._create_label('<b>Bar behaviour</b>')
+        self._create_check_box('stopOnAnswer', 'Stop drain on answer shown')
+        self._create_check_box('disableAddon', 'Disable Life Drain (!)')
+        self._create_label('<b>Bar style</b>')
+        self._create_combo_box('positionList', 'Position', POSITION_OPTIONS)
+        self._create_spin_box('heightInput', 'Height', [1, 40])
+        self._create_spin_box('borderRadiusInput', 'Border radius', [0, 20])
+        self._create_combo_box('textList', 'Text', TEXT_OPTIONS)
+        self._create_combo_box('styleList', 'Style', STYLE_OPTIONS)
+        self._create_color_select('bgColor', 'Background color')
+        self._create_color_select('fgColor', 'Foreground color')
+        self._create_color_select('textColor', 'Text color')
+        self._fill_remaining_space()
         form.tabWidget.addTab(form.lifedrain_widget, 'Life Drain')
 
     def preferences_load(self, pref):
@@ -127,47 +119,39 @@ class Settings(object):
         '''
         Appends a new tab to deck settings dialog.
         '''
+        self._form = form
+        self._row = 0
+
         form.lifedrain_widget = self._qt.QWidget()
         form.lifedrain_layout = self._gui_settings_setup_layout(form.lifedrain_widget)
-        row = 0
         self._create_label(
-            form, row,
             'The <b>maximum life</b> is the time in seconds for the life bar go '
             'from full to empty.\n<b>Recover</b> is the time in seconds that is '
             'recovered after answering a card. <b>Damage</b> is the life lost '
             'when a card is answered with \'Again\'.'
         )
-        row += 1
-        self._create_spin_box(form, row, 'maxLifeInput', 'Maximum life', [1, 10000])
-        row += 1
-        self._create_spin_box(form, row, 'recoverInput', 'Recover', [0, 1000])
-        row += 1
-        self._create_check_box(form, row, 'enableDamageInput', 'Enable damage')
-        row += 1
-        self._create_spin_box(form, row, 'damageInput', 'Damage', [-1000, 1000])
-        row += 1
-        self._create_spin_box(form, row, 'currentValueInput', 'Current life', [0, 10000])
-        row += 1
-        self._fill_remaining_space(form, row)
+        self._create_spin_box('maxLifeInput', 'Maximum life', [1, 10000])
+        self._create_spin_box('recoverInput', 'Recover', [0, 1000])
+        self._create_check_box('enableDamageInput', 'Enable damage')
+        self._create_spin_box('damageInput', 'Damage', [-1000, 1000])
+        self._create_spin_box('currentValueInput', 'Current life', [0, 10000])
+        self._fill_remaining_space()
         form.tabWidget.addTab(form.lifedrain_widget, 'Life Drain')
 
     def custom_deck_settings_ui(self, form, is_anki21):
         '''
         Adds LifeDrain configurations to custom study dialog.
         '''
+        self._form = form
+        self._row = 0
+
         form.lifedrain_widget = self._qt.QGroupBox('Life Drain')
         form.lifedrain_layout = self._gui_settings_setup_layout(form.lifedrain_widget)
-        row = 0
-        self._create_spin_box(form, row, 'maxLifeInput', 'Maximum life', [1, 10000])
-        row += 1
-        self._create_spin_box(form, row, 'recoverInput', 'Recover', [0, 1000])
-        row += 1
-        self._create_check_box(form, row, 'enableDamageInput', 'Enable damage')
-        row += 1
-        self._create_spin_box(form, row, 'damageInput', 'Damage', [-1000, 1000])
-        row += 1
-        self._create_spin_box(form, row, 'currentValueInput', 'Current life', [0, 10000])
-        row += 1
+        self._create_spin_box('maxLifeInput', 'Maximum life', [1, 10000])
+        self._create_spin_box('recoverInput', 'Recover', [0, 1000])
+        self._create_check_box('enableDamageInput', 'Enable damage')
+        self._create_spin_box('damageInput', 'Damage', [-1000, 1000])
+        self._create_spin_box('currentValueInput', 'Current life', [0, 10000])
         index = 3 if is_anki21 else 2
         form.verticalLayout.insertWidget(index, form.lifedrain_widget)
 
@@ -183,47 +167,51 @@ class Settings(object):
         layout.setColumnMinimumWidth(2, 50)
         return layout
 
-    def _create_label(self, form, row, text, color=None):
+    def _create_label(self, text, color=None):
         '''
         Creates a label that occupies the whole line and wraps if it is too big.
         '''
         label = self._qt.QLabel(text)
         label.setWordWrap(True)
-        form.lifedrain_layout.addWidget(label, row, 0, 1, 4)
+        self._form.lifedrain_layout.addWidget(label, self._row, 0, 1, 4)
         if color:
             label.setStyleSheet('color: {}'.format(color))
+        self._row += 1
 
-    def _create_combo_box(self, form, row, cb_name, label_text, options):
+    def _create_combo_box(self, cb_name, label_text, options):
         '''
         Creates a combo box with the specified label and options.
         '''
         label = self._qt.QLabel(label_text)
-        setattr(form, cb_name, self._qt.QComboBox(form.lifedrain_widget))
+        setattr(self._form, cb_name, self._qt.QComboBox(self._form.lifedrain_widget))
         for option in options:
-            getattr(form, cb_name).addItem(option)
-        form.lifedrain_layout.addWidget(label, row, 0)
-        form.lifedrain_layout.addWidget(getattr(form, cb_name), row, 2, 1, 2)
+            getattr(self._form, cb_name).addItem(option)
+        self._form.lifedrain_layout.addWidget(label, self._row, 0)
+        self._form.lifedrain_layout.addWidget(getattr(self._form, cb_name), self._row, 2, 1, 2)
+        self._row += 1
 
-    def _create_check_box(self, form, row, cb_name, label_text):
+    def _create_check_box(self, cb_name, label_text):
         '''
         Creates a checkbox with the specified label.
         '''
         label = self._qt.QLabel(label_text)
-        setattr(form, cb_name, self._qt.QCheckBox(form.lifedrain_widget))
-        form.lifedrain_layout.addWidget(label, row, 0)
-        form.lifedrain_layout.addWidget(getattr(form, cb_name), row, 2, 1, 2)
+        setattr(self._form, cb_name, self._qt.QCheckBox(self._form.lifedrain_widget))
+        self._form.lifedrain_layout.addWidget(label, self._row, 0)
+        self._form.lifedrain_layout.addWidget(getattr(self._form, cb_name), self._row, 2, 1, 2)
+        self._row += 1
 
-    def _create_spin_box(self, form, row, sb_name, label_text, val_range):
+    def _create_spin_box(self, sb_name, label_text, val_range):
         '''
         Creates a spin box with the specified label and range.
         '''
         label = self._qt.QLabel(label_text)
-        setattr(form, sb_name, self._qt.QSpinBox(form.lifedrain_widget))
-        getattr(form, sb_name).setRange(val_range[0], val_range[1])
-        form.lifedrain_layout.addWidget(label, row, 0)
-        form.lifedrain_layout.addWidget(getattr(form, sb_name), row, 2, 1, 2)
+        setattr(self._form, sb_name, self._qt.QSpinBox(self._form.lifedrain_widget))
+        getattr(self._form, sb_name).setRange(val_range[0], val_range[1])
+        self._form.lifedrain_layout.addWidget(label, self._row, 0)
+        self._form.lifedrain_layout.addWidget(getattr(self._form, sb_name), self._row, 2, 1, 2)
+        self._row += 1
 
-    def _create_color_select(self, form, row, cs_name, label_text):
+    def _create_color_select(self, cs_name, label_text):
         '''
         Creates a color select with the specified label.
         '''
@@ -231,27 +219,29 @@ class Settings(object):
         select_button = self._qt.QPushButton('Select')
         cs_preview_name = '%sPreview' % cs_name
         cs_dialog_name = '%sDialog' % cs_name
-        setattr(form, cs_preview_name, self._qt.QLabel(''))
-        setattr(form, cs_dialog_name, self._qt.QColorDialog(select_button))
-        getattr(form, cs_dialog_name).setOption(self._qt.QColorDialog.DontUseNativeDialog)
+        setattr(self._form, cs_preview_name, self._qt.QLabel(''))
+        setattr(self._form, cs_dialog_name, self._qt.QColorDialog(select_button))
+        getattr(self._form, cs_dialog_name).setOption(self._qt.QColorDialog.DontUseNativeDialog)
         select_button.pressed.connect(
             lambda: self._select_color_dialog(
-                getattr(form, cs_dialog_name),
-                getattr(form, cs_preview_name)
+                getattr(self._form, cs_dialog_name),
+                getattr(self._form, cs_preview_name)
             )
         )
-        form.lifedrain_layout.addWidget(label, row, 0)
-        form.lifedrain_layout.addWidget(select_button, row, 2)
-        form.lifedrain_layout.addWidget(getattr(form, cs_preview_name), row, 3)
+        self._form.lifedrain_layout.addWidget(label, self._row, 0)
+        self._form.lifedrain_layout.addWidget(select_button, self._row, 2)
+        self._form.lifedrain_layout.addWidget(getattr(self._form, cs_preview_name), self._row, 3)
+        self._row += 1
 
-    def _fill_remaining_space(self, form, row):
+    def _fill_remaining_space(self):
         '''
         Fills the remaining space, so what comes after this is in the bottom.
         '''
         spacer = self._qt.QSpacerItem(
             1, 1, self._qt.QSizePolicy.Minimum, self._qt.QSizePolicy.Expanding
         )
-        form.lifedrain_layout.addItem(spacer, row, 0)
+        self._form.lifedrain_layout.addItem(spacer, self._row, 0)
+        self._row += 1
 
     @staticmethod
     def _select_color_dialog(qcolor_dialog, preview_label):
