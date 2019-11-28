87332e3c9deffc7890cd94618f9f5e2a9fdc6ddf
Yutsuten <mateus.etto@gmail.com>
2019-11-28 14:27:17 +0900

    Ignore pylint error on wrapper function

--- lifedrain/decorators.py
+++ lifedrain/decorators.py
@@ -10,7 +10,7 @@ def must_be_enabled(func):
     '''
     Runs the method only if the add-on is enabled.
     '''
-    def wrapper(self, *args, **kwargs):
+    def wrapper(self, *args, **kwargs):  # pylint: disable=missing-docstring
         if self.main_window.col is None or \
                 self.main_window.col.conf.get('disable', DEFAULTS['disable']) is True:
             return None
