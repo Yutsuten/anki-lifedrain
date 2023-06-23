6bf23551e0c955ee0d288b938c8d4d445005e35a
Yutsuten <mateus.etto@gmail.com>
2023-06-24 00:41:43 +0900

    Add copyright to exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,6 @@
+# Copyright (c) Yutsuten <https://github.com/Yutsuten>. Licensed under AGPL-3.0.
+# See the LICENCE file in the repository root for full licence text.
+
 class LifedrainError(Exception):
     """Lifedrain base exception."""
     def __init__(self, message: str):
