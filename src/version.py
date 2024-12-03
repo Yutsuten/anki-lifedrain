e301ed7241a202a6e10cf30e608adbda10370ea2
Mateus Eto <mateus.etto@gmail.com>
2024-12-03 15:40:06 +0900

    Version 2.8.3

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Yutsuten <https://github.com/Yutsuten>. Licensed under AGPL-3.0.
 # See the LICENCE file in the repository root for full licence text.
 
-VERSION = '2.8.2'
+VERSION = '2.8.3'
